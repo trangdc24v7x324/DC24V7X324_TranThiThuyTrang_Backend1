57ae80e059fcac2694e1e4f75b6bc1a2f0de1af8
trangdc24v7x324 <trangdc24v7x324@dttx.ctu.edu.vn>
2025-10-31 15:55:39 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+const contactsRouter = require("./app/routes/contact.route");
+app.use("/api/contacts", contactsRouter);
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
